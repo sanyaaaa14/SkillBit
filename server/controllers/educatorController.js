c7cd7d361af0199dc31988cf0e122c6dc23e3ccd
Sanya Bhatia <bhatiasanya1410@gmail.com>
2026-03-27 16:45:38 +0530

    educator controller changes

--- server/controllers/educatorController.js
+++ server/controllers/educatorController.js
@@ -1,19 +1,25 @@
 import { clerkClient } from '@clerk/express'
 
-//Update Role to Educator 
 
-export const updateRoleToEducator = async (req,res) => {
+export const updateRoleToEducator = async (req, res) => {
   try {
-    const userId = req.auth.userId
+    console.log("AUTH:", req.auth);
+    console.log("USER ID:", req.auth?.userId);
+
+    const userId = req.auth?.userId;
+
+    if (!userId) {
+      return res.json({ success: false, message: "No userId found" });
+    }
 
     await clerkClient.users.updateUserMetadata(userId, {
       publicMetadata: {
         role: 'educator',
       },
-    })
+    });
 
-    res.json({ success: true, message: 'You can publish a course now' })
+    res.json({ success: true, message: 'You can publish a course now' });
   } catch (error) {
-    res.json({success:false ,message:error.message})
+    res.json({ success: false, message: error.message });
   }
-}
+};
