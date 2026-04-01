06366bd8e6b59ec8cdd6658c9760d2c56fe8f7cb
Sanya Bhatia <bhatiasanya1410@gmail.com>
2026-04-01 18:26:27 +0530

    API error fix

--- server/configs/cloudinary.js
+++ server/configs/cloudinary.js
@@ -1,11 +1,25 @@
-import {v2 as cloudinary} from 'cloudinary'
+import { v2 as cloudinary } from 'cloudinary';
 
-const connectCloudinary= async()=>{
+const connectCloudinary = async () => {
     cloudinary.config({
-        cloud_name: process.env.CLOUDINARY_NAME,
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
         api_key: process.env.CLOUDINARY_API_KEY,
-        api_secret: process.env.CLOUDINARY_SECRET_KEY
+        api_secret: process.env.CLOUDINARY_API_SECRET
     });
 };
 
 export default connectCloudinary;
+
+
+
+// import {v2 as cloudinary} from 'cloudinary'
+
+// const connectCloudinary= async()=>{
+//     cloudinary.config({
+//         cloud_name: process.env.CLOUDINARY_NAME,
+//         api_key: process.env.CLOUDINARY_API_KEY,
+//         api_secret: process.env.CLOUDINARY_SECRET_KEY
+//     });
+// };
+
+// export default connectCloudinary;
